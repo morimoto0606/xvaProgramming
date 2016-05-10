f09e609294881819fec48f4f5769ac57557fccda
morimoto0606 <morimoto0606@yahoo.co.jp>
2016-05-11 01:31:24 +0900

    Signed-off-by: morimoto0606 <morimoto0606@yahoo.co.jp>

--- UnitTest/CvaByAnalyticExposureTest.cpp
+++ UnitTest/CvaByAnalyticExposureTest.cpp
@@ -66,5 +66,32 @@ namespace UnitTest
 			Assert::AreEqual(6.64204, cva2.deriv(), 1e-2);
 		}
 
+		TEST_METHOD(CvaAnalyticExposureAsian)
+		{
+			cva::Asian payoff(1.0, 120.0);
+			cva::Dual<double> x(100, 1.0);
+			cva::Dual<double> sigma(0.3);
+			cva::Dual<double> mu(0.0);
+
+			const double maturity = 10.0;
+			const std::size_t gridNum = 10;
+			const std::size_t pathNum = 1000;
+			const double dt = maturity / gridNum;
+			std::size_t seed = 1;
+			const cva::Path<cva::Dual<double>> path(
+				x, mu, sigma, pathNum, gridNum, dt, seed);
+
+			cva::Dual<double> cva1 = cva::calcCvaByAnalyticExposure(
+				path, payoff, cva::ExplicitCalculator{});
+
+			Assert::AreEqual(374.765, cva1.value(), 1e-2);
+			Assert::AreEqual(6.98343, cva1.deriv(), 1e-2);
+
+			cva::Dual<double> cva2 = cva::calcCvaByAnalyticExposure(
+				path, payoff, cva::ImplicitCalculator{});
+
+			Assert::AreEqual(350.204, cva2.value(), 1e-2);
+			Assert::AreEqual(6.64204, cva2.deriv(), 1e-2);
+		}
 	};
 }
